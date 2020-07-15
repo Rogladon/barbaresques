989fc1ea6a11f8884edfa825ffcd0406d0134c04
Sergey Naumov <drmgc@yandex.ru>
2020-07-15 13:31:03 +0300

    wip

--- Assets/Scripts/Battle/Systems/BattleGameSystemGroup.cs
+++ Assets/Scripts/Battle/Systems/BattleGameSystemGroup.cs
@@ -1,5 +1,4 @@
 using Unity.Entities;
-using Unity.Physics;
 
 namespace Barbaresques.Battle {
 	[UpdateInGroup(typeof(SimulationSystemGroup), OrderFirst = true), UpdateAfter(typeof(BeginSimulationEntityCommandBufferSystem))]
