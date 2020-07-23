259a27c454719f45f0db9e3ccd582b5509745b96
Sergey Naumov <drmgc@yandex.ru>
2020-07-23 15:23:31 +0300

    impr(WalkingSystem): нормальное хождение

--- Assets/Scripts/Battle/Systems/LevelSimulation/Unit/WalkingSystem.cs
+++ Assets/Scripts/Battle/Systems/LevelSimulation/Unit/WalkingSystem.cs
@@ -24,28 +24,19 @@ namespace Barbaresques.Battle {
 
 			Entities.WithName("walk")
 				.ForEach((int entityInQueryIndex, Entity e, ref Translation translation, ref Rotation rotation, in Walking walking, in Speed speed) => {
-					var currentSpeed = speed.value;
-					if (walking.speedFactor == 0) {
-						// FIXME: так-то, все эти репорты можно обернуть в статический метод с [BurstDiscard], но тогда для запуска в редакторе придётся запускать без burst компиляции
-						// TODO: как-то триггериться на 0
-						// Debug.LogError($"{nameof(walking.speedFactor)} of entity {e} was set to 0, which must NOT be used and will be treated as 1. If you want to \"pause\" walking, use {nameof(Walking)}.{nameof(Walking.SPEED_FACTOR_ZERO)} constant");
-					} else if (walking.speedFactor < 0) {
-						currentSpeed = 0;
-					} else if (walking.speedFactor > 1) {
-						// TODO: стоит вообще предупреждать, если speedFactor > 1?
-						// Debug.LogWarning($"{nameof(walking.speedFactor)} of entity {e} was set to {walking.speedFactor}, which is >= 1, which is weired. If you want to walk speed faster, then set in {nameof(Speed)} use buffs.");
-					} else {
-						currentSpeed *= walking.speedFactor;
-					}
-
 					var diff = walking.target - translation.Value;
 					var len = length(diff);
-					var n = normalize(diff);
 
-					if (len > 0.25f)
-						translation.Value += n * min(len, currentSpeed * delta);
-					else
+					if (len > 0.25f) {
+						var targetRotation = quaternion.AxisAngle(new float3(0, 1, 0), -atan2(diff.z, diff.x) + atan2(1, 0));
+						rotation.Value = slerp(rotation.Value, targetRotation, delta * 2);
+
+						var currentSpeed = min(len, math.clamp(speed.value * walking.speedFactor * delta, 0.0f, 1.0f));
+
+						translation.Value += forward(rotation.Value) * currentSpeed;
+					} else {
 						ecb.RemoveComponent<Walking>(entityInQueryIndex, e);
+					}
 				})
 				.ScheduleParallel();
 
