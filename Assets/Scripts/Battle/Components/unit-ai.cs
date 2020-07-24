1b2a58695f2e8a7ae62a0bdc320ef0c8a20f93a9
Sergey Naumov <drmgc@yandex.ru>
2020-07-24 11:02:45 +0300

    rm(battle/UnitAI): удаление старого ИИ

--- Assets/Scripts/Battle/Components/unit-ai.cs
+++ Assets/Scripts/Battle/Components/unit-ai.cs
@@ -4,37 +4,4 @@ namespace Barbaresques.Battle {
 	[GenerateAuthoringComponent]
 	public struct UnitAi : IComponentData {
 	}
-
-	public enum UnitAiStates {
-		[AssociatedComponent(typeof(UnitAiStateIdle))]
-		IDLE,
-		[AssociatedComponent(typeof(UnitAiStateGoTo))]
-		GO_TO,
-		[AssociatedComponent(typeof(UnitAiStateAttack))]
-		ATTACK,
-		[AssociatedComponent(typeof(UnitAiStateRetreat))]
-		RETREAT,
-	}
-
-	public struct UnitAiState : ISystemStateComponentData {
-		public UnitAiStates state;
-	}
-
-	public struct UnitAiStateSwitch : ISystemStateComponentData {
-		public UnitAiStates previousState;
-		public UnitAiStates newState;
-
-		/// <summary>
-		/// Просто инициализировать ИИ для юнита?
-		/// </summary>
-		public bool initialization => previousState == newState;
-	}
-
-	public struct UnitAiStateIdle : ISystemStateComponentData {}
-
-	public struct UnitAiStateGoTo : ISystemStateComponentData {}
-
-	public struct UnitAiStateAttack : ISystemStateComponentData {}
-
-	public struct UnitAiStateRetreat : ISystemStateComponentData {}
 }
